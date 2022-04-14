c38b4627da0cbd257b916a411280950e418b31b7
Josh <2015jtw@gmail.com>
2022-04-13 20:05:49 -0400

    finished arrays section

--- JS/conditionalsAndIfStatements.js
+++ JS/conditionalsAndIfStatements.js
@@ -20,4 +20,22 @@ else {
     console.log("you are a senior. You pay $10")
 }
 
+// using nested if statements
+
+const password = prompt("please enter a new password");
+
+// password must be +6 characters
+if (password.length > 6) {
+
+    // password cant include spaces
+    if (password.indexOf(' ') === -1) {
+        console.log("good job, you have a valid password");
+    }
+    else {
+        console.log("password can't contain spaces")
+    }
+}
+else {
+    console.log("password too short")
+}
 
