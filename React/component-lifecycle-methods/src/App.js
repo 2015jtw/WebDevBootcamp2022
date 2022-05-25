5e5c293008794b4240829d68cc40198c5c80bc8c
Josh <2015jtw@gmail.com>
2022-05-25 18:58:42 -0400

    started lifecycle methods

---
+++ React/component-lifecycle-methods/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+// import components
+import Timer from './Timer';
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
